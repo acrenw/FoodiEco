57c1b4eadc92881716a14238a7d39a5ce47b4f1c
acrenw <75736027+acrenw@users.noreply.github.com>
2022-06-24 10:00:40 -0400

    Create removeFood.py

---
+++ removeFood.py
@@ -0,0 +1,157 @@
+def removeFood(event):
+	x = event.x
+	y = event.x
+  
+	if 100 <= y <= 150:
+		if 100 <= x <= 150:
+			s.delete(labels[0])
+			labels[0] = ''
+			s.delete(icons[0])
+		elif 150 <= x <= 200:
+			s.delete(labels[1])
+			labels[1] = ''  
+			s.delete(icons[1])
+		elif 200 <= x <= 250:
+			s.delete(labels[2])
+			labels[2] = ''
+			s.delete(icons[2])
+		elif 250 <= x <= 300:
+			s.delete(labels[3])
+			labels[3] = ''
+			s.delete(icons[3])
+		elif 300 <= x <= 350:
+			s.delete(labels[4])
+			labels[4] = ''
+			s.delete(icons[4])
+		
+	elif 200 <= y <= 250:
+		if 100 <= x <= 150:
+			s.delete(labels[5])
+			labels[5] = ''
+			s.delete(icons[5])
+		elif 150 <= x <= 200:
+			s.delete(labels[6])
+			labels[6] = ''
+			s.delete(icons[6])
+		elif 200 <= x <= 250:
+			s.delete(labels[7])
+			labels[7] = ''
+			s.delete(icons[7])
+		elif 250 <= x <= 300:
+			s.delete(labels[8])
+			labels[8] = ''
+			s.delete(icons[8])
+		elif 300 <= x <= 350:
+			s.delete(labels[9])
+			labels[9] = ''
+			s.delete(icons[9])
+		
+	elif 275 <= y <= 350:
+		if 100 <= x <= 150:
+			s.delete(labels[10])
+			labels[10] = ''
+			s.delete(icons[10])
+		elif 150 <= x <= 200:
+			s.delete(labels[11])
+			labels[11] = ''
+			s.delete(icons[11])
+		elif 200 <= x <= 250:
+			s.delete(labels[12])
+			labels[12] = ''
+			s.delete(icons[12])
+		elif 250 <= x <= 300:
+			s.delete(labels[13])
+			labels[13] = ''
+			s.delete(icons[13])
+		elif 300 <= x <= 350:
+			s.delete(labels[14])
+			labels[14] = ''
+			s.delete(icons[14])
+		
+	elif 375 <= y <= 450:	
+		if 100 <= x <= 150:
+			s.delete(labels[15])
+			labels[15] = ''
+			s.delete(icons[15])
+		elif 150 <= x <= 200:
+			s.delete(labels[16])
+			labels[16] = ''
+			s.delete(icons[16])
+		elif 200 <= x <= 250:
+			s.delete(labels[17])
+			labels[17] = ''
+			s.delete(icons[17])
+		elif 250 <= x <= 300:
+			s.delete(labels[18])
+			labels[18] = ''
+			s.delete(icons[18])
+		elif 300 <= x <= 350:
+			s.delete(labels[19])
+			labels[19] = ''
+			s.delete(icons[19])
+		
+	elif 475 <= y <= 550:
+		if 100 <= x <= 150:
+			s.delete(labels[20])
+			labels[20] = ''
+			s.delete(icons[20])
+		elif 150 <= x <= 200:
+			s.delete(labels[21])
+			labels[21] = ''
+			s.delete(icons[21])
+		elif 200 <= x <= 250:
+			s.delete(labels[22])
+			labels[22] = ''
+			s.delete(icons[22])
+		elif 250 <= x <= 300:
+			s.delete(labels[23])
+			labels[23] = ''
+			s.delete(icons[23])
+		elif 300 <= x <= 350:
+			s.delete(labels[24])
+			labels[24] = ''
+			s.delete(icons[24])
+	
+	elif 575 <= y <= 625:
+		if 100 <= x <= 150:
+			s.delete(labels[25])
+			labels[25] = ''
+			s.delete(icons[25])
+		elif 150 <= x <= 200:
+			s.delete(labels[26])
+			labels[26] = ''
+			s.delete(icons[26])
+		elif 200 <= x <= 250:
+			s.delete(labels[27])
+			labels[27] = ''
+			s.delete(icons[27])
+		elif 250 <= x <= 300:
+			s.delete(labels[28])
+			labels[28] = ''
+			s.delete(icons[28])
+		elif 300 <= x <= 350:
+			s.delete(labels[29])
+			labels[29] = ''
+			s.delete(icons[29])
+	
+	elif 650 <= y <= 725:
+		if 100 <= x <= 150:
+			s.delete(labels[30])
+			labels[30] = ''
+			s.delete(icons[30])
+		elif 150 <= x <= 200:
+			s.delete(labels[31])
+			labels[31] = ''
+			s.delete(icons[31])
+		elif 200 <= x <= 250:
+			s.delete(labels[32])
+			labels[32] = ''
+			s.delete(icons[32])
+		elif 250 <= x <= 300:
+			s.delete(labels[33])
+			labels[33] = ''
+			s.delete(icons[33])
+		elif 300 <= x <= 350:
+			s.delete(labels[34])
+			labels[34] = ''
+			s.delete(icons[34])
